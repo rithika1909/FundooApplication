19b173d7cb61b2be3639bddc768581d4b764ec49
Rithika L <rithika.l@tvsnext.com>
2023-10-12 17:08:24 +0530

    [Rithika]Added

---
+++ FundooManager/IManager/IUserManager.cs
@@ -0,0 +1,17 @@
+﻿using FundooModel.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FundooManager.IManager
+{
+    public interface IUserManager
+    {
+        public Task<int> RegisterUser(Register register);
+        public Register LoginUser(Login login);
+        public Register ResetPassword(ResetPassword reset);
+    }
+}
